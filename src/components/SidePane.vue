583e078ef38f69ea0a16ac38ee3c623deb2c09b6
Martin Maillard <self@martin-maillard.com>
2020-05-25 11:06:42 +0200

    Add half-baked side-pane component

---
+++ src/components/SidePane.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="quickview" :class="{ 'is-active': isOpen }">
+    <header class="quickview-header">
+      <p class="title">Side pane title</p>
+      <button class="delete" @click="$emit('close')"></button>
+    </header>
+
+    <div class="quickview-body">
+      <div class="quickview-block">
+        ...
+      </div>
+    </div>
+    <footer class="quickview-footer"></footer>
+  </div>
+</template>
+
+<script>
+import Quickview from 'bulma-quickview/dist/js/bulma-quickview'
+import 'bulma-quickview/dist/css/bulma-quickview.min.css'
+
+export default {
+  name: 'SidePane',
+  props: ['isOpen'],
+
+  data () {
+    return {
+      quickview: null
+    }
+  },
+
+  mounted () {
+    this.$nextTick(() => {
+      this.quickview = new Quickview(this.$el, {})
+    })
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="home">
+    <h1>Hello</h1>
+
+    <button class="button is-primary" @click="sidePaneOpen = !sidePaneOpen">Open</button>
+
+    <SidePane :isOpen="sidePaneOpen" @close="sidePaneOpen = false" />
   </div>
 </template>
 
 <script>
+import SidePane from '@/components/SidePane.vue'
+
 export default {
   name: 'Home',
-  components: {}
+  components: {
+    SidePane
+  },
+
+  data () {
+    return {
+      sidePaneOpen: false
+    }
+  }
 }
 </script>

a08ec8301415e2a33c65131b2818349397933226
Martin Maillard <self@martin-maillard.com>
2020-05-26 17:10:17 +0200

    Add missing ButtonDelete component

---
+++ src/components/ButtonDelete.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-tooltip :label="title" type="is-light" :delay="200" size="is-small">
+    <b-button type="is-white" class="has-text-grey" size="is-small" icon-left="trash" v-bind="$attrs" v-on="$listeners">
+      <slot />
+    </b-button>
+  </b-tooltip>
+</template>
+
+<script>
+export default {
+  name: 'ButtonDelete',
+  props: ['title']
+}
+</script>

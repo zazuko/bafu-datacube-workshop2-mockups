a48ebfb38b96bc0824afb9d8a8b7f6f033f48154
Martin Maillard <self@martin-maillard.com>
2020-06-02 12:24:32 +0200

    Refactor buttons

--- src/components/ButtonDelete.vue
+++ src/components/ButtonDelete.vue
@@ -1,14 +1,12 @@
 <template>
-  <b-tooltip :label="title" type="is-light" :delay="200" size="is-small">
-    <b-button type="is-white" class="has-text-grey" size="is-small" icon-left="trash" v-bind="$attrs" v-on="$listeners">
-      <slot />
-    </b-button>
-  </b-tooltip>
+  <Button type="is-white has-text-grey" size="is-small" icon="trash" v-bind="$attrs" v-on="$listeners" />
 </template>
 
 <script>
+import Button from './Button.vue'
+
 export default {
   name: 'ButtonDelete',
-  props: ['title']
+  components: { Button },
 }
 </script>

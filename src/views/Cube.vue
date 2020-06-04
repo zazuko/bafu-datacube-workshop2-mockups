07c628c5a080dbb734df285052d427aab157d950
Martin Maillard <self@martin-maillard.com>
2020-06-04 09:21:25 +0200

    Re-add mapper/designer links

--- src/views/Cube.vue
+++ src/views/Cube.vue
@@ -1,5 +1,20 @@
 <template>
   <section class="Cube">
+    <div class="tabs">
+      <ul>
+        <router-link :to="{ name: 'Mapper' }" v-slot="{ href,  isActive }">
+          <li :class="{ 'is-active': isActive }">
+            <a :href="href">Mapper</a>
+          </li>
+        </router-link>
+        <router-link :to="{ name: 'Designer' }" v-slot="{ href,  isActive }">
+          <li :class="{ 'is-active': isActive }">
+            <a :href="href">Designer</a>
+          </li>
+        </router-link>
+      </ul>
+    </div>
+
     <router-view />
   </section>
 </template>
@@ -11,13 +26,20 @@
   height: 100vh;
   overflow-y: hidden;
 }
+
+.tabs {
+  margin-bottom: 0 !important;
+  min-height: 41px;
+}
 </style>
 
 <script>
+import ResourceLabel from '@/components/ResourceLabel'
 import data from '@/data'
 
 export default {
   name: 'Cube',
+  components: { ResourceLabel },
 
   data () {
     return {
